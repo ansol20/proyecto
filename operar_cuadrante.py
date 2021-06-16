b8ea4701c27f436ce5b60b60dc7b550bb1f2f4c8
ansol20 <rssvedra@gmail.com>
2021-06-16 08:47:23 -0500

    v1

--- operar_cuadrante.py
+++ operar_cuadrante.py
@@ -23,31 +23,44 @@ cuadrantes = json.load(f)
 clusters = cuadrantes["clusters"]
 enlaces = []
 for posicion in range(0,len(clusters)):
-  enlace = {"centro":clusters[posicion]["coordenada_red"], "alrededores":[]}
+  enlace = {"coordenada_red":clusters[posicion]["coordenada_red"],"coordenadas":clusters[posicion]["cluster"],"hospitales":[], "cluster_alrededores":[]}
+  for n in range(0,len(clusters[posicion]["hospitales"])):
+    enlace["hospitales"].append({"nombre":clusters[posicion]["hospitales"][n]["nombre"],"coordenadas":clusters[posicion]["hospitales"][n]["localizacion"],"distancia":distanceInKmBetweenEarthCoordinates(clusters[posicion]["cluster"],clusters[posicion]["hospitales"][n]["localizacion"])})
   if clusters[posicion]["coordenada_red"][0]+1<=48:
     if clusters[posicion]["coordenada_red"][1]-1>=0:
+      posicion_vector = (clusters[posicion]["coordenada_red"][0]+1)*49+clusters[posicion]["coordenada_red"][1]-1
+      enlace_alrededor = {"coordenada_red":[clusters[posicion]["coordenada_red"][0]+1,clusters[posicion]["coordenada_red"][1]-1],"distancia":distanceInKmBetweenEarthCoordinates(clusters[posicion]["cluster"],clusters[posicion_vector]["cluster"]),"coordenadas":clusters[posicion_vector]["cluster"],"hospitales":[]}
+      for n in range(0,len(clusters[posicion_vector]["hospitales"])):
+        enlace_alrededor["hospitales"].append({"nombre":clusters[posicion_vector]["hospitales"][n]["nombre"],"coordenadas":clusters[posicion_vector]["hospitales"][n]["localizacion"],"distancia":distanceInKmBetweenEarthCoordinates(clusters[posicion_vector]["cluster"],clusters[posicion_vector]["hospitales"][n]["localizacion"])})
       #agregar hospitales por punto y la distancia al cluster
       #posicion 0, es la x, posicion 1, es la y, posicion 3 es la distancia en km.
       #Encontramos el cuadrante el hospital, a partir del cuadrante, buscamos todos los clusters y hospitales en los cluster al rededor. 
-      enlace["alrededores"].append([clusters[posicion]["coordenada_red"][0]+1,clusters[posicion]["coordenada_red"][1]-1,distanceInKmBetweenEarthCoordinates(clusters[posicion]["cluster"],clusters[(clusters[posicion]["coordenada_red"][0]+1)*49+clusters[posicion]["coordenada_red"][1]-1]["cluster"])])
-    enlace["alrededores"].append([clusters[posicion]["coordenada_red"][0]+1,clusters[posicion]["coordenada_red"][1],distanceInKmBetweenEarthCoordinates(clusters[posicion]["cluster"],clusters[(clusters[posicion]["coordenada_red"][0]+1)*49+clusters[posicion]["coordenada_red"][1]]["cluster"])])
+      enlace["cluster_alrededores"].append(enlace_alrededor)
+    enlace["cluster_alrededores"].append({"coordenada_red":[clusters[posicion]["coordenada_red"][0]+1,clusters[posicion]["coordenada_red"][1]],"distancia":distanceInKmBetweenEarthCoordinates(clusters[posicion]["cluster"],clusters[(clusters[posicion]["coordenada_red"][0]+1)*49+clusters[posicion]["coordenada_red"][1]]["cluster"]),"coordenadas":clusters[(clusters[posicion]["coordenada_red"][0]+1)*49+clusters[posicion]["coordenada_red"][1]]["cluster"]})
     if clusters[posicion]["coordenada_red"][1]+1<=48:
-      enlace["alrededores"].append([clusters[posicion]["coordenada_red"][0]+1,clusters[posicion]["coordenada_red"][1]+1,distanceInKmBetweenEarthCoordinates(clusters[posicion]["cluster"],clusters[(clusters[posicion]["coordenada_red"][0]+1)*49+clusters[posicion]["coordenada_red"][1]+1]["cluster"])])
+      enlace["cluster_alrededores"].append({"coordenada_red":[clusters[posicion]["coordenada_red"][0]+1,clusters[posicion]["coordenada_red"][1]+1],"distancia":distanceInKmBetweenEarthCoordinates(clusters[posicion]["cluster"],clusters[(clusters[posicion]["coordenada_red"][0]+1)*49+clusters[posicion]["coordenada_red"][1]+1]["cluster"]),"coordenadas":clusters[(clusters[posicion]["coordenada_red"][0]+1)*49+clusters[posicion]["coordenada_red"][1]+1]["cluster"]})
   if clusters[posicion]["coordenada_red"][1]-1>=0:
-    enlace["alrededores"].append([clusters[posicion]["coordenada_red"][0],clusters[posicion]["coordenada_red"][1]-1,distanceInKmBetweenEarthCoordinates(clusters[posicion]["cluster"],clusters[(clusters[posicion]["coordenada_red"][0])*49+clusters[posicion]["coordenada_red"][1]-1]["cluster"])])
+    enlace["cluster_alrededores"].append({"coordenada_red":[clusters[posicion]["coordenada_red"][0],clusters[posicion]["coordenada_red"][1]-1],"distancia":distanceInKmBetweenEarthCoordinates(clusters[posicion]["cluster"],clusters[(clusters[posicion]["coordenada_red"][0])*49+clusters[posicion]["coordenada_red"][1]-1]["cluster"]),"coordenadas":clusters[(clusters[posicion]["coordenada_red"][0])*49+clusters[posicion]["coordenada_red"][1]-1]["cluster"]})
+
   if clusters[posicion]["coordenada_red"][1]+1<=48:
-    enlace["alrededores"].append([clusters[posicion]["coordenada_red"][0],clusters[posicion]["coordenada_red"][1]+1,distanceInKmBetweenEarthCoordinates(clusters[posicion]["cluster"],clusters[(clusters[posicion]["coordenada_red"][0])*49+clusters[posicion]["coordenada_red"][1]+1]["cluster"])])
+    posicion_vector = (clusters[posicion]["coordenada_red"][1]+1)*49+clusters[posicion]["coordenada_red"][1]+1
+    enlace_alrededor = {"coordenada_red":[clusters[posicion]["coordenada_red"][1]+1,clusters[posicion]["coordenada_red"][1]+1],"distancia":distanceInKmBetweenEarthCoordinates(clusters[posicion]["cluster"],clusters[posicion_vector]["cluster"]),"coordenadas":clusters[posicion_vector]["cluster"],"hospitales":[]}
+    for n in range(0,len(clusters[posicion_vector]["hospitales"])):
+        enlace_alrededor["hospitales"].append({"nombre":clusters[posicion_vector]["hospitales"][n]["nombre"],"coordenadas":clusters[posicion_vector]["hospitales"][n]["localizacion"],"distancia":distanceInKmBetweenEarthCoordinates(clusters[posicion_vector]["cluster"],clusters[posicion_vector]["hospitales"][n]["localizacion"])})
+    #enlace["cluster_alrededores"].append({"coordenada_red":[clusters[posicion]["coordenada_red"][0],clusters[posicion]["coordenada_red"][1]+1],"distancia":distanceInKmBetweenEarthCoordinates(clusters[posicion]["cluster"],clusters[(clusters[posicion]["coordenada_red"][0])*49+clusters[posicion]["coordenada_red"][1]+1]["cluster"]),"coordenadas":clusters[(clusters[posicion]["coordenada_red"][0])*49+clusters[posicion]["coordenada_red"][1]+1]["cluster"]})
   if clusters[posicion]["coordenada_red"][0]-1>=0:
     if clusters[posicion]["coordenada_red"][1]-1>=0:
-      enlace["alrededores"].append([clusters[posicion]["coordenada_red"][0]-1,clusters[posicion]["coordenada_red"][1]-1,distanceInKmBetweenEarthCoordinates(clusters[posicion]["cluster"],clusters[(clusters[posicion]["coordenada_red"][0]-1)*49+clusters[posicion]["coordenada_red"][1]-1]["cluster"])])
-    enlace["alrededores"].append([clusters[posicion]["coordenada_red"][0]-1,clusters[posicion]["coordenada_red"][1],distanceInKmBetweenEarthCoordinates(clusters[posicion]["cluster"],clusters[(clusters[posicion]["coordenada_red"][0]-1)*49+clusters[posicion]["coordenada_red"][1]]["cluster"])])
+      enlace["cluster_alrededores"].append({"coordenada_red":[clusters[posicion]["coordenada_red"][0]-1,clusters[posicion]["coordenada_red"][1]-1],"distancia":distanceInKmBetweenEarthCoordinates(clusters[posicion]["cluster"],clusters[(clusters[posicion]["coordenada_red"][0]-1)*49+clusters[posicion]["coordenada_red"][1]-1]["cluster"]),"coordenadas":clusters[(clusters[posicion]["coordenada_red"][0]-1)*49+clusters[posicion]["coordenada_red"][1]-1]["cluster"]})
+    enlace["cluster_alrededores"].append({"coordenada_red":[clusters[posicion]["coordenada_red"][0]-1,clusters[posicion]["coordenada_red"][1]],"distancia":distanceInKmBetweenEarthCoordinates(clusters[posicion]["cluster"],clusters[(clusters[posicion]["coordenada_red"][0]-1)*49+clusters[posicion]["coordenada_red"][1]]["cluster"]),"coordenadas":clusters[(clusters[posicion]["coordenada_red"][0]-1)*49+clusters[posicion]["coordenada_red"][1]]["cluster"]})
     if clusters[posicion]["coordenada_red"][1]+1<=48:
-      enlace["alrededores"].append([clusters[posicion]["coordenada_red"][0]-1,clusters[posicion]["coordenada_red"][1]+1,distanceInKmBetweenEarthCoordinates(clusters[posicion]["cluster"],clusters[(clusters[posicion]["coordenada_red"][0]-1)*48+clusters[posicion]["coordenada_red"][1]+1]["cluster"])])   
+      posicion_vector = (clusters[posicion]["coordenada_red"][1]+1)*49+clusters[posicion]["coordenada_red"][1]+1
+    enlace_alrededor = {"coordenada_red":[clusters[posicion]["coordenada_red"][1]+1,clusters[posicion]["coordenada_red"][1]+1],"distancia":distanceInKmBetweenEarthCoordinates(clusters[posicion]["cluster"],clusters[posicion_vector]["cluster"]),"coordenadas":clusters[posicion_vector]["cluster"],"hospitales":[]}
+    for n in range(0,len(clusters[posicion_vector]["hospitales"])):
+        enlace_alrededor["hospitales"].append({"nombre":clusters[posicion_vector]["hospitales"][n]["nombre"],"coordenadas":clusters[posicion_vector]["hospitales"][n]["localizacion"],"distancia":distanceInKmBetweenEarthCoordinates(clusters[posicion_vector]["cluster"],clusters[posicion_vector]["hospitales"][n]["localizacion"])})
+      #enlace["cluster_alrededores"].append({"coordenada_red":[clusters[posicion]["coordenada_red"][0]-1,clusters[posicion]["coordenada_red"][1]+1],"distancia":distanceInKmBetweenEarthCoordinates(clusters[posicion]["cluster"],clusters[(clusters[posicion]["coordenada_red"][0]-1)*49+clusters[posicion]["coordenada_red"][1]+1]["cluster"]),"coordenadas":clusters[(clusters[posicion]["coordenada_red"][0]-1)*49+clusters[posicion]["coordenada_red"][1]+1]["cluster"]})   
   enlaces.append(enlace)
-#print(clusters[0],clusters[1],clusters[49])
 print(enlaces)
-
-with open('enlaces.json', 'w') as outfile:
+with open('/Users/Ansol/Desktop/enlaces.json', 'w') as outfile:
     json.dump(enlaces, outfile)
 #
 # 
